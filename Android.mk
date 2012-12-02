70e6161de95ed938d5366a3036f13270d80144da
SunRain <41245110@qq.com>
2012-12-02 19:32:36 +0800

    Add static twrp recovery build
    Change-Id: I65d353b2765fc16b31655f692cfbb22f1bf0f7fd

--- Android.mk
+++ Android.mk
@@ -14,11 +14,13 @@
 
 LOCAL_PATH := $(call my-dir)
 
+twrp_local_path := $(LOCAL_PATH)
+
 TARGET_RECOVERY_GUI := true
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
+twrp_sources := \
     recovery.cpp \
     twbootloader.cpp \
     install.cpp \
@@ -29,7 +31,7 @@ LOCAL_SRC_FILES := \
     fixPermissions.cpp \
     adb_install.cpp
 
-LOCAL_SRC_FILES += \
+twrp_sources += \
     data.cpp \
     makelist.cpp \
     partition.cpp \
@@ -39,46 +41,172 @@ LOCAL_SRC_FILES += \
     twrp-functions.cpp \
     openrecoveryscript.cpp
 
+EXTRA_C_INCLUDES += \
+    bionic \
+    external/stlport/stlport \
+    system/extras/ext4_utils
+
+RECOVERY_API_VERSION := 3
+EXTRA_CFLAGS += -DRECOVERY_API_VERSION=$(RECOVERY_API_VERSION)
+
 ifneq ($(TARGET_RECOVERY_REBOOT_SRC),)
-  LOCAL_SRC_FILES += $(TARGET_RECOVERY_REBOOT_SRC)
+  twrp_sources += $(TARGET_RECOVERY_REBOOT_SRC)
 endif
+
+ifeq ($(TARGET_RECOVERY_UI_LIB),)
+  twrp_sources += default_device.cpp
+else
+  EXTRA_STATIC_LIBRARIES += $(TARGET_RECOVERY_UI_LIB)
+endif
+
+ifeq ($(TARGET_USERIMAGES_USE_EXT4), true)
+    EXTRA_CFLAGS += -DUSE_EXT4
+    EXTRA_C_INCLUDES += system/extras/ext4_utils
+endif
+
+ifeq ($(HAVE_SELINUX), true)
+  EXTRA_C_INCLUDES += external/libselinux/include
+  EXTRA_STATIC_LIBRARIES += libselinux
+  EXTRA_CFLAGS += -DHAVE_SELINUX
+endif # HAVE_SELINUX
+
+#TWRP Build Flags
+ifeq ($(BOARD_HAS_NO_REAL_SDCARD), true)
+    EXTRA_CFLAGS += -DBOARD_HAS_NO_REAL_SDCARD
+endif
+ifneq ($(SP1_NAME),)
+	EXTRA_CFLAGS += -DSP1_NAME=$(SP1_NAME) -DSP1_BACKUP_METHOD=$(SP1_BACKUP_METHOD) -DSP1_MOUNTABLE=$(SP1_MOUNTABLE)
+endif
+ifneq ($(SP1_DISPLAY_NAME),)
+	EXTRA_CFLAGS += -DSP1_DISPLAY_NAME=$(SP1_DISPLAY_NAME)
+endif
+ifneq ($(SP2_NAME),)
+	EXTRA_CFLAGS += -DSP2_NAME=$(SP2_NAME) -DSP2_BACKUP_METHOD=$(SP2_BACKUP_METHOD) -DSP2_MOUNTABLE=$(SP2_MOUNTABLE)
+endif
+ifneq ($(SP2_DISPLAY_NAME),)
+	EXTRA_CFLAGS += -DSP2_DISPLAY_NAME=$(SP2_DISPLAY_NAME)
+endif
+ifneq ($(SP3_NAME),)
+	EXTRA_CFLAGS += -DSP3_NAME=$(SP3_NAME) -DSP3_BACKUP_METHOD=$(SP3_BACKUP_METHOD) -DSP3_MOUNTABLE=$(SP3_MOUNTABLE)
+endif
+ifneq ($(SP3_DISPLAY_NAME),)
+	EXTRA_CFLAGS += -DSP3_DISPLAY_NAME=$(SP3_DISPLAY_NAME)
+endif
+ifneq ($(RECOVERY_SDCARD_ON_DATA),)
+	EXTRA_CFLAGS += -DRECOVERY_SDCARD_ON_DATA
+endif
+ifneq ($(TW_INCLUDE_DUMLOCK),)
+	EXTRA_CFLAGS += -DTW_INCLUDE_DUMLOCK
+endif
+ifneq ($(TW_INTERNAL_STORAGE_PATH),)
+	EXTRA_CFLAGS += -DTW_INTERNAL_STORAGE_PATH=$(TW_INTERNAL_STORAGE_PATH)
+endif
+ifneq ($(TW_INTERNAL_STORAGE_MOUNT_POINT),)
+	EXTRA_CFLAGS += -DTW_INTERNAL_STORAGE_MOUNT_POINT=$(TW_INTERNAL_STORAGE_MOUNT_POINT)
+endif
+ifneq ($(TW_EXTERNAL_STORAGE_PATH),)
+	EXTRA_CFLAGS += -DTW_EXTERNAL_STORAGE_PATH=$(TW_EXTERNAL_STORAGE_PATH)
+endif
+ifneq ($(TW_EXTERNAL_STORAGE_MOUNT_POINT),)
+	EXTRA_CFLAGS += -DTW_EXTERNAL_STORAGE_MOUNT_POINT=$(TW_EXTERNAL_STORAGE_MOUNT_POINT)
+endif
+ifeq ($(TW_HAS_NO_RECOVERY_PARTITION), true)
+    EXTRA_CFLAGS += -DTW_HAS_NO_RECOVERY_PARTITION
+endif
+ifeq ($(TW_NO_REBOOT_BOOTLOADER), true)
+    EXTRA_CFLAGS += -DTW_NO_REBOOT_BOOTLOADER
+endif
+ifeq ($(TW_NO_REBOOT_RECOVERY), true)
+    EXTRA_CFLAGS += -DTW_NO_REBOOT_RECOVERY
+endif
+ifeq ($(TW_NO_BATT_PERCENT), true)
+    EXTRA_CFLAGS += -DTW_NO_BATT_PERCENT
+endif
+ifneq ($(TW_CUSTOM_POWER_BUTTON),)
+	EXTRA_CFLAGS += -DTW_CUSTOM_POWER_BUTTON=$(TW_CUSTOM_POWER_BUTTON)
+endif
+ifeq ($(TW_ALWAYS_RMRF), true)
+    EXTRA_CFLAGS += -DTW_ALWAYS_RMRF
+endif
+ifeq ($(TW_NEVER_UNMOUNT_SYSTEM), true)
+    EXTRA_CFLAGS += -DTW_NEVER_UNMOUNT_SYSTEM
+endif
+ifeq ($(TW_NO_USB_STORAGE), true)
+    EXTRA_CFLAGS += -DTW_NO_USB_STORAGE
+endif
+ifeq ($(TW_INCLUDE_INJECTTWRP), true)
+    EXTRA_CFLAGS += -DTW_INCLUDE_INJECTTWRP
+endif
+ifeq ($(TW_INCLUDE_BLOBPACK), true)
+    EXTRA_CFLAGS += -DTW_INCLUDE_BLOBPACK
+endif
+ifeq ($(TW_DEFAULT_EXTERNAL_STORAGE), true)
+    EXTRA_CFLAGS += -DTW_DEFAULT_EXTERNAL_STORAGE
+endif
+ifneq ($(TARGET_USE_CUSTOM_LUN_FILE_PATH),)
+    EXTRA_CFLAGS += -DCUSTOM_LUN_FILE=\"$(TARGET_USE_CUSTOM_LUN_FILE_PATH)\"
+endif
+ifneq ($(BOARD_UMS_LUNFILE),)
+    EXTRA_CFLAGS += -DCUSTOM_LUN_FILE=\"$(BOARD_UMS_LUNFILE)\"
+endif
+#ifeq ($(TW_FLASH_FROM_STORAGE), true) Making this the default behavior
+    EXTRA_CFLAGS += -DTW_FLASH_FROM_STORAGE
+#endif
+ifeq ($(TW_HAS_DOWNLOAD_MODE), true)
+    EXTRA_CFLAGS += -DTW_HAS_DOWNLOAD_MODE
+endif
+ifeq ($(TW_SDEXT_NO_EXT4), true)
+    EXTRA_CFLAGS += -DTW_SDEXT_NO_EXT4
+endif
+ifeq ($(TW_FORCE_CPUINFO_FOR_DEVICE_ID), true)
+    EXTRA_CFLAGS += -DTW_FORCE_CPUINFO_FOR_DEVICE_ID
+endif
+ifeq ($(TW_INCLUDE_CRYPTO), true)
+    EXTRA_CFLAGS += -DTW_INCLUDE_CRYPTO
+    EXTRA_CFLAGS += -DCRYPTO_FS_TYPE=\"$(TW_CRYPTO_FS_TYPE)\"
+    EXTRA_CFLAGS += -DCRYPTO_REAL_BLKDEV=\"$(TW_CRYPTO_REAL_BLKDEV)\"
+    EXTRA_CFLAGS += -DCRYPTO_MNT_POINT=\"$(TW_CRYPTO_MNT_POINT)\"
+    EXTRA_CFLAGS += -DCRYPTO_FS_OPTIONS=\"$(TW_CRYPTO_FS_OPTIONS)\"
+    EXTRA_CFLAGS += -DCRYPTO_FS_FLAGS=\"$(TW_CRYPTO_FS_FLAGS)\"
+    EXTRA_CFLAGS += -DCRYPTO_KEY_LOC=\"$(TW_CRYPTO_KEY_LOC)\"
+    EXTRA_SHARED_LIBRARIES += libcrypto
+    twrp_sources += crypto/ics/cryptfs.c
+    EXTRA_C_INCLUDES += system/extras/ext4_utils external/openssl/include
+endif
+ifeq ($(TW_INCLUDE_JB_CRYPTO), true)
+    EXTRA_CFLAGS += -DTW_INCLUDE_CRYPTO
+    EXTRA_CFLAGS += -DTW_INCLUDE_JB_CRYPTO
+    EXTRA_SHARED_LIBRARIES += libcrypto
+    EXTRA_STATIC_LIBRARIES += libfs_mgrtwrp
+    twrp_sources += crypto/jb/cryptfs.c
+    EXTRA_C_INCLUDES += system/extras/ext4_utils external/openssl/include
+endif
+
+###########################################
+# Build dynamically linked recovery
+###########################################
+
+ifneq ($(BUILD_TWRP_STANDALONE),1)
+
+include $(CLEAR_VARS)
 
 LOCAL_MODULE := recovery
 
+LOCAL_SRC_FILES := $(twrp_sources)
+LOCAL_C_INCLUDES += ${EXTRA_C_INCLUDES}
+LOCAL_CFLAGS += ${EXTRA_CFLAGS}
+
 #LOCAL_FORCE_STATIC_EXECUTABLE := true
-
-RECOVERY_API_VERSION := 3
-LOCAL_CFLAGS += -DRECOVERY_API_VERSION=$(RECOVERY_API_VERSION)
-
-#LOCAL_STATIC_LIBRARIES := \
-#    libext4_utils \
-#    libminzip \
-#    libmtdutils \
-#    libmincrypt \
-#    libminadbd \
-#    libpixelflinger_static
-
-LOCAL_C_INCLUDES += bionic external/stlport/stlport
 
 LOCAL_STATIC_LIBRARIES :=
 LOCAL_SHARED_LIBRARIES :=
 
+LOCAL_STATIC_LIBRARIES += ${EXTRA_STATIC_LIBRARIES}
 LOCAL_STATIC_LIBRARIES += libmtdutils
 LOCAL_STATIC_LIBRARIES += libminadbd libminzip libunz
 LOCAL_STATIC_LIBRARIES += libminuitwrp libpixelflinger_static libpng libjpegtwrp libgui
 LOCAL_SHARED_LIBRARIES += libz libc libstlport libcutils libstdc++ libmincrypt libext4_utils
-
-ifeq ($(TARGET_USERIMAGES_USE_EXT4), true)
-    LOCAL_CFLAGS += -DUSE_EXT4
-    LOCAL_C_INCLUDES += system/extras/ext4_utils
-    #LOCAL_STATIC_LIBRARIES += libext4_utils
-endif
-
-ifeq ($(HAVE_SELINUX), true)
-  LOCAL_C_INCLUDES += external/libselinux/include
-  LOCAL_STATIC_LIBRARIES += libselinux
-  LOCAL_CFLAGS += -DHAVE_SELINUX
-endif # HAVE_SELINUX
+LOCAL_SHARED_LIBRARIES += ${EXTRA_SHARED_LIBRARIES}
 
 # This binary is in the recovery ramdisk, which is otherwise a copy of root.
 # It gets copied there in config/Makefile.  LOCAL_MODULE_TAGS suppresses
@@ -86,127 +214,52 @@ endif # HAVE_SELINUX
 # TODO: Build the ramdisk image in a more principled way.
 LOCAL_MODULE_TAGS := eng
 
-ifeq ($(TARGET_RECOVERY_UI_LIB),)
-  LOCAL_SRC_FILES += default_device.cpp
-else
-  LOCAL_STATIC_LIBRARIES += $(TARGET_RECOVERY_UI_LIB)
-endif
+include $(BUILD_EXECUTABLE)
 
-LOCAL_C_INCLUDES += system/extras/ext4_utils
+endif #!BUILD_TWRP_STANDALONE
 
-#TWRP Build Flags
-ifeq ($(BOARD_HAS_NO_REAL_SDCARD), true)
-    LOCAL_CFLAGS += -DBOARD_HAS_NO_REAL_SDCARD
-endif
-ifneq ($(SP1_NAME),)
-	LOCAL_CFLAGS += -DSP1_NAME=$(SP1_NAME) -DSP1_BACKUP_METHOD=$(SP1_BACKUP_METHOD) -DSP1_MOUNTABLE=$(SP1_MOUNTABLE)
-endif
-ifneq ($(SP1_DISPLAY_NAME),)
-	LOCAL_CFLAGS += -DSP1_DISPLAY_NAME=$(SP1_DISPLAY_NAME)
-endif
-ifneq ($(SP2_NAME),)
-	LOCAL_CFLAGS += -DSP2_NAME=$(SP2_NAME) -DSP2_BACKUP_METHOD=$(SP2_BACKUP_METHOD) -DSP2_MOUNTABLE=$(SP2_MOUNTABLE)
-endif
-ifneq ($(SP2_DISPLAY_NAME),)
-	LOCAL_CFLAGS += -DSP2_DISPLAY_NAME=$(SP2_DISPLAY_NAME)
-endif
-ifneq ($(SP3_NAME),)
-	LOCAL_CFLAGS += -DSP3_NAME=$(SP3_NAME) -DSP3_BACKUP_METHOD=$(SP3_BACKUP_METHOD) -DSP3_MOUNTABLE=$(SP3_MOUNTABLE)
-endif
-ifneq ($(SP3_DISPLAY_NAME),)
-	LOCAL_CFLAGS += -DSP3_DISPLAY_NAME=$(SP3_DISPLAY_NAME)
-endif
-ifneq ($(RECOVERY_SDCARD_ON_DATA),)
-	LOCAL_CFLAGS += -DRECOVERY_SDCARD_ON_DATA
-endif
-ifneq ($(TW_INCLUDE_DUMLOCK),)
-	LOCAL_CFLAGS += -DTW_INCLUDE_DUMLOCK
-endif
-ifneq ($(TW_INTERNAL_STORAGE_PATH),)
-	LOCAL_CFLAGS += -DTW_INTERNAL_STORAGE_PATH=$(TW_INTERNAL_STORAGE_PATH)
-endif
-ifneq ($(TW_INTERNAL_STORAGE_MOUNT_POINT),)
-	LOCAL_CFLAGS += -DTW_INTERNAL_STORAGE_MOUNT_POINT=$(TW_INTERNAL_STORAGE_MOUNT_POINT)
-endif
-ifneq ($(TW_EXTERNAL_STORAGE_PATH),)
-	LOCAL_CFLAGS += -DTW_EXTERNAL_STORAGE_PATH=$(TW_EXTERNAL_STORAGE_PATH)
-endif
-ifneq ($(TW_EXTERNAL_STORAGE_MOUNT_POINT),)
-	LOCAL_CFLAGS += -DTW_EXTERNAL_STORAGE_MOUNT_POINT=$(TW_EXTERNAL_STORAGE_MOUNT_POINT)
-endif
-ifeq ($(TW_HAS_NO_RECOVERY_PARTITION), true)
-    LOCAL_CFLAGS += -DTW_HAS_NO_RECOVERY_PARTITION
-endif
-ifeq ($(TW_NO_REBOOT_BOOTLOADER), true)
-    LOCAL_CFLAGS += -DTW_NO_REBOOT_BOOTLOADER
-endif
-ifeq ($(TW_NO_REBOOT_RECOVERY), true)
-    LOCAL_CFLAGS += -DTW_NO_REBOOT_RECOVERY
-endif
-ifeq ($(TW_NO_BATT_PERCENT), true)
-    LOCAL_CFLAGS += -DTW_NO_BATT_PERCENT
-endif
-ifneq ($(TW_CUSTOM_POWER_BUTTON),)
-	LOCAL_CFLAGS += -DTW_CUSTOM_POWER_BUTTON=$(TW_CUSTOM_POWER_BUTTON)
-endif
-ifeq ($(TW_ALWAYS_RMRF), true)
-    LOCAL_CFLAGS += -DTW_ALWAYS_RMRF
-endif
-ifeq ($(TW_NEVER_UNMOUNT_SYSTEM), true)
-    LOCAL_CFLAGS += -DTW_NEVER_UNMOUNT_SYSTEM
-endif
-ifeq ($(TW_NO_USB_STORAGE), true)
-    LOCAL_CFLAGS += -DTW_NO_USB_STORAGE
-endif
-ifeq ($(TW_INCLUDE_INJECTTWRP), true)
-    LOCAL_CFLAGS += -DTW_INCLUDE_INJECTTWRP
-endif
-ifeq ($(TW_INCLUDE_BLOBPACK), true)
-    LOCAL_CFLAGS += -DTW_INCLUDE_BLOBPACK
-endif
-ifeq ($(TW_DEFAULT_EXTERNAL_STORAGE), true)
-    LOCAL_CFLAGS += -DTW_DEFAULT_EXTERNAL_STORAGE
-endif
-ifneq ($(TARGET_USE_CUSTOM_LUN_FILE_PATH),)
-    LOCAL_CFLAGS += -DCUSTOM_LUN_FILE=\"$(TARGET_USE_CUSTOM_LUN_FILE_PATH)\"
-endif
-ifneq ($(BOARD_UMS_LUNFILE),)
-    LOCAL_CFLAGS += -DCUSTOM_LUN_FILE=\"$(BOARD_UMS_LUNFILE)\"
-endif
-#ifeq ($(TW_FLASH_FROM_STORAGE), true) Making this the default behavior
-    LOCAL_CFLAGS += -DTW_FLASH_FROM_STORAGE
-#endif
-ifeq ($(TW_HAS_DOWNLOAD_MODE), true)
-    LOCAL_CFLAGS += -DTW_HAS_DOWNLOAD_MODE
-endif
-ifeq ($(TW_SDEXT_NO_EXT4), true)
-    LOCAL_CFLAGS += -DTW_SDEXT_NO_EXT4
-endif
-ifeq ($(TW_FORCE_CPUINFO_FOR_DEVICE_ID), true)
-    LOCAL_CFLAGS += -DTW_FORCE_CPUINFO_FOR_DEVICE_ID
-endif
-ifeq ($(TW_INCLUDE_CRYPTO), true)
-    LOCAL_CFLAGS += -DTW_INCLUDE_CRYPTO
-    LOCAL_CFLAGS += -DCRYPTO_FS_TYPE=\"$(TW_CRYPTO_FS_TYPE)\"
-    LOCAL_CFLAGS += -DCRYPTO_REAL_BLKDEV=\"$(TW_CRYPTO_REAL_BLKDEV)\"
-    LOCAL_CFLAGS += -DCRYPTO_MNT_POINT=\"$(TW_CRYPTO_MNT_POINT)\"
-    LOCAL_CFLAGS += -DCRYPTO_FS_OPTIONS=\"$(TW_CRYPTO_FS_OPTIONS)\"
-    LOCAL_CFLAGS += -DCRYPTO_FS_FLAGS=\"$(TW_CRYPTO_FS_FLAGS)\"
-    LOCAL_CFLAGS += -DCRYPTO_KEY_LOC=\"$(TW_CRYPTO_KEY_LOC)\"
-    LOCAL_SHARED_LIBRARIES += libcrypto
-    LOCAL_SRC_FILES += crypto/ics/cryptfs.c
-    LOCAL_C_INCLUDES += system/extras/ext4_utils external/openssl/include
-endif
-ifeq ($(TW_INCLUDE_JB_CRYPTO), true)
-    LOCAL_CFLAGS += -DTW_INCLUDE_CRYPTO
-    LOCAL_CFLAGS += -DTW_INCLUDE_JB_CRYPTO
-    LOCAL_SHARED_LIBRARIES += libcrypto
-    LOCAL_STATIC_LIBRARIES += libfs_mgrtwrp
-    LOCAL_SRC_FILES += crypto/jb/cryptfs.c
-    LOCAL_C_INCLUDES += system/extras/ext4_utils external/openssl/include
-endif
+###########################################
+# Build static linked recovery
+###########################################
+
+ifeq ($(BUILD_TWRP_STANDALONE),1)
+
+LOCAL_PATH := $(twrp_local_path)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := recovery
+#LOCAL_MODULE_STEM := recovery-static
+
+LOCAL_SRC_FILES := $(twrp_sources)
+LOCAL_C_INCLUDES += ${EXTRA_C_INCLUDES}
+LOCAL_CFLAGS += ${EXTRA_CFLAGS}
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_STATIC_LIBRARIES :=
+LOCAL_SHARED_LIBRARIES :=
+
+LOCAL_STATIC_LIBRARIES += ${EXTRA_STATIC_LIBRARIES} ${EXTRA_SHARED_LIBRARIES}
+LOCAL_STATIC_LIBRARIES += libminzip libunz libmincrypt
+LOCAL_STATIC_LIBRARIES += libminizip libminadbd libedify libbusybox libmkyaffs2image libunyaffs liberase_image libdump_image libflash_image
+LOCAL_STATIC_LIBRARIES += libdedupe libcrypto_static libcrecovery libflashutils libmtdutils libmmcutils libbmlutils
+LOCAL_STATIC_LIBRARIES += libminuitwrp libpixelflinger_static libpng libjpegtwrp libgui libminui
+LOCAL_STATIC_LIBRARIES += libz libc libstlport_static libcutils libstdc++  libext4_utils
+
+RECOVERY_LINKS := edify busybox flash_image dump_image mkyaffs2image unyaffs erase_image nandroid reboot volume setprop dedupe minizip
+
+# This binary is in the recovery ramdisk, which is otherwise a copy of root.
+# It gets copied there in config/Makefile.  LOCAL_MODULE_TAGS suppresses
+# a (redundant) copy of the binary in /system/bin for user builds.
+# TODO: Build the ramdisk image in a more principled way.
+LOCAL_MODULE_TAGS := eng
 
 include $(BUILD_EXECUTABLE)
+
+endif #BUILD_TWRP_STANDALONE
+
+###########################################
+###########################################
 
 include $(CLEAR_VARS)
 # Create busybox symlinks... gzip and gunzip are excluded because those need to link to pigz instead
@@ -218,7 +271,7 @@ $(RECOVERY_BUSYBOX_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Symlink: $@ -> $(BUSYBOX_BINARY)"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf $(BUSYBOX_BINARY) $@
+	$(hide) ln -s $(BUSYBOX_BINARY) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(RECOVERY_BUSYBOX_SYMLINKS)
 
